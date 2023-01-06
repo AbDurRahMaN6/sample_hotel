3482e537f291e683d9df9c4b98db921b55438ca9
Abdurrahman <abdurrahmanms5258@gmail.com>
2023-01-06 11:43:20 +0530

    All Changes in Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,6 @@
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { red } from "@material-ui/core/colors";
 import { Link } from "react-router-dom";
 import "./header.css";
 
@@ -39,9 +40,6 @@ const Header = () => {
             <button className="dropBtn" type="button" style={{width: "170px"}}>Rooms & Suites<span className="arrow"></span></button>
             <div className="ddContent" 
             style={{marginLeft: "-160px", marginTop: "0px"}}>
-              {/* <Link className="linking">Link 1</Link>
-              <Link className="linking">Link 2</Link>
-              <Link className="linking">Link 3</Link> */}
               <div className="row">
         <div className="column">
           <h4>Rooms</h4>
@@ -66,8 +64,19 @@ const Header = () => {
           <Link className="roomLink">Executive Suite & Premier Ocean Room Connecting</Link>
           <Link className="roomLink">Two Deluxe Lake View Rooms Connecting</Link>
         </div>
+        <hr  style={{
+          marginTop: "20px",
+          width: "1180px",
+    color: '#000000',
+    backgroundColor: '#000000',
+    height: .5,
+    borderColor : '#000000',
+    marginLeft: "-10px"
+}}/>
+
+<Link className="roomLink" style={{marginLeft: "-10px"}}>ROOMS & SUITES OVERVIEW</Link>
       </div>
-      <hr />
+
             </div>
           </div>
           <div className="dropdown">
@@ -75,11 +84,36 @@ const Header = () => {
             style={{width: "120px"}} 
             >Dinning<span className="arrow"></span></button>
             <div className="ddContent" 
-            style={{float: "left", marginLeft: "-330px"}}
-            >
-              <Link className="linking">Link 1</Link>
-              <Link className="linking">Link 2</Link>
-              <Link className="linking">Link 3</Link>
+            style={{float: "left", marginLeft: "-330px"}}>
+
+<div className="row">
+        <div className="column">
+          <h4>Restaurants</h4>
+          <Link className="roomLink">Cake Temptations by Shangri-La Colombo</Link>
+          <Link className="roomLink">Shang Palace</Link>
+          <Link className="roomLink">Capital Bar & Grill</Link>
+          <Link className="roomLink">Central</Link>
+        </div>
+        <div class="column">
+          <h4>Bars & Lounges</h4>
+          <Link className="roomLink">Sapphyr Lounge</Link>
+          <Link className="roomLink">Capital Bar & Grill</Link>
+          <Link className="roomLink">Pool Bar</Link>
+        </div>
+
+        <hr  style={{
+          marginTop: "20px",
+          width: "1180px",
+    color: '#000000',
+    backgroundColor: '#000000',
+    height: .5,
+    borderColor : '#000000',
+    marginLeft: "-10px"
+}}/>
+
+<Link className="roomLink" style={{marginLeft: "-10px"}}>DINING OVERVIEW</Link>
+        </div>
+
             </div>
           </div>
           <div className="dropdown">
@@ -89,9 +123,21 @@ const Header = () => {
             <div className="ddContent" 
             style={{marginLeft: "-450px"}}
             >
-              <Link className="linking">Link 1</Link>
-              <Link className="linking">Link 2</Link>
-              <Link className="linking">Link 3</Link>
+              <div className="row">
+        <div className="column">
+          <h4>For Kids</h4>
+          <Link className="roomLink">Overview</Link>
+          <Link className="roomLink">Adventure Zone</Link>
+          <Link className="roomLink">Celebrations</Link>
+        </div>
+
+        <div class="column">
+          <h4>Health & Leisure</h4>
+          <Link className="roomLink">Overview</Link>
+          <Link className="roomLink">Health Club</Link>
+          <Link className="roomLink">Chi, The Spa</Link>
+        </div>
+        </div>
             </div>
           </div>
 
@@ -100,9 +146,30 @@ const Header = () => {
             <div className="ddContent" 
             style={{marginLeft: "-600px"}}
             >
-              <Link className="linking">Link 1</Link>
-              <Link className="linking">Link 2</Link>
-              <Link className="linking">Link 3</Link>
+             
+
+
+
+
+
+<div className="row">
+        <div className="column">
+          <h4>Meetings & Events</h4>
+          <Link className="roomLink">Overview</Link>
+          <Link className="roomLink">Experience It</Link>
+          <Link className="roomLink">Meet Work Create</Link>
+          <Link className="roomLink">Outside Catering</Link>
+          <Link className="roomLink">Request For Proposal</Link>
+        </div>
+
+        <div class="column">
+          <h4>Wedding and Celebrations</h4>
+          <Link className="roomLink">Overview</Link>
+          <Link className="roomLink">Wedding Planning</Link>
+          <Link className="roomLink">Weddings By Shangri-La</Link>
+          <Link className="roomLink">Request for Proposal</Link>
+        </div>
+        </div>
             </div>
           </div>
           <div className="dropdown">
@@ -116,9 +183,14 @@ const Header = () => {
             <div className="ddContent" 
             style={{marginLeft: "-940px"}}
             >
-              <Link className="linking">Link 1</Link>
-              <Link className="linking">Link 2</Link>
-              <Link className="linking">Link 3</Link>
+              <div className="row">
+        <div className="column">
+          <h4>Apartments</h4>
+          <Link className="roomLink">Learn More</Link>
+        </div>
+        </div>
+              
+
             </div>
           </div>
           
